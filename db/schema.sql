c3739bf6ec673459d2d4093e1920b46927212dfd
LindseyIsaac <isaaclindsey89@gmail.com>
2023-06-06 12:53:25 -0500

    stuff added in the db

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,33 @@
-stuff
+-- database set up 
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+USE employees_db;
+
+-- department table
+CREATE TABLE dept (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    deptName VARCHAR(30) NOT NULL
+);
+
+-- role table
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title VARCHAR (30),
+    salary DECIMAL,
+    deptID INT,
+    FOREIGN KEY (deptID)
+    REFERENCES dept(id)
+);
+
+-- employee table
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    firstName VARCHAR(30) NOT NULL,
+    lastName VARCHAR(30) NOT NULL,
+    roleID INT,
+    FOREIGN KEY (roleID)
+    REFERENCES role(id),
+    managerID INT,
+    FOREIGN KEY (managerID)
+    REFERENCES employee(id)
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,33 +1,28 @@
-use employees;
+-- Seed data for application 
 
-INSERT INTO department
-    (name)
-VALUES
-    ('Sales'),
-    ('Engineering'),
-    ('Finance'),
-    ('Legal');
+INSERT INTO department(department_name)
+VALUES  ('Sales'),
+        ('Engineering'),
+        ('Legal'),
+        ('Finance');
 
-INSERT INTO role
-    (title, salary, department_id)
-VALUES
-    ('Sales Lead', 100000, 1),
-    ('Salesperson', 80000, 1),
-    ('Lead Engineer', 150000, 2),
-    ('Software Engineer', 120000, 2),
-    ('Account Manager', 160000, 3),
-    ('Accountant', 125000, 3),
-    ('Legal Team Lead', 250000, 4),
-    ('Lawyer', 190000, 4);
+Insert INTO role(title, salary, department_id)
+VALUES  ('Sales Lead', 100000, 1),
+        ('Sales Person', 80000, 1),
+        ('Lead Engineer', 60000, 2),
+        ('Software Engineer', 120000, 2),
+        ('Legal Team Lead', 250000, 3),
+        ('lawyer', 190000, 3),
+        ('Account Manager', 160000, 4),
+        ('Accountant', 125000, 4);
 
-INSERT INTO employee
-    (first_name, last_name, role_id, manager_id)
-VALUES
-    ('John', 'Doe', 1, NULL),
-    ('Mike', 'Chan', 2, 1),
-    ('Ashley', 'Rodriguez', 3, NULL),
-    ('Kevin', 'Tupik', 4, 3),
-    ('Kunal', 'Singh', 5, NULL),
-    ('Malia', 'Brown', 6, 5),
-    ('Sarah', 'Lourd', 7, NULL),
-    ('Tom', 'Allen', 8, 7);
+Insert INTO employee(first_name, last_name, role_id, manager_id)
+VALUES ('John', 'Smith', 1, NULL),
+        ('Ann', 'Taylor', 5, 1 ),
+        ('Mike', 'Jackson', 2, NULL),
+        ('Brad', 'Pittus', 6, 2),
+        ('Lonny', 'Johnson', 3, NULL),
+        ('Tilly', 'Parker', 7, 3),
+        ('Mathew', 'McCons', 4, NULL),
+        ('Benny', 'Bookman', 8, 4);
+        
